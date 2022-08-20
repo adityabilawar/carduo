836154f261f03485fcb9dc5ec50dbbe6ffd1f81c
aaronbiscotti <thesoupmanguy@gmail.com>
2022-08-20 14:06:25 -0400

    login

--- components/Register.tsx
+++ components/Register.tsx
@@ -2,7 +2,31 @@ import React, { useContext } from 'react'
 import Link from "next/link"
 const Register = () => {
     return (
-        <>
+<div className="flex justify-around">
+            <div className="flex justify-center items-center h-screen">
+                <div className="flex flex-col items-start space-y-8">
+                    <div className="flex flex-col">
+                        <p className="mb-3">Transform your study habits</p>
+                        <h1 className="text-5xl font-bold">Sign in</h1>
+                    </div>
+                    <div className="flex flex-col space-y-8">
+                        <div>
+                            <p>Username</p>
+                            <input type="text" placeholder="Username" />
+                        </div>
+                        <div>
+                            <p>Password</p>
+                            <input type="text" placeholder="Username" />
+                        </div>
+                        <Link href="/dashboard">
+                            <button className='btn text-white'>
+                                Sign in
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="w-[2px] z-[-110] h-screen bg-gray-200" />
             <div className="flex justify-center items-center h-screen">
                 <div className="flex flex-col items-start space-y-8">
                     <div className="flex flex-col">
@@ -23,13 +47,10 @@ const Register = () => {
                                 Register
                             </button>
                         </Link>
-                        <Link href="/login">
-                            <p>Already have an account? <span className="cursor-pointer text-[#F43A3B]">Sign in here</span></p>
-                        </Link>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
--- components/UnauthNavbar.tsx
+++ components/UnauthNavbar.tsx
@@ -8,7 +8,7 @@ const menuItems = {
 
 const UnauthNavbar = () => {
   return (
-    <div className="left-0 right-0 top-0 h-40 shadow flex px-10 fixed justify-between items-center">
+    <div className="left-0 right-0 top-0 h-40 shadow flex px-10 fixed justify-between items-center bg-white">
         <div className="flex justify-center items-center space-x-3">
             <img src="/logo.svg" />
             <h1 className="text-2xl font-bold">Carduo</h1>
