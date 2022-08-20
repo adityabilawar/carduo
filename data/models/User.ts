e01e6a3ef75267a5745ebc6965e9503668039a1a
voomp <bat7gamer7@gmail.com>
2022-08-20 13:20:00 -0400

    fixing typescript bug

--- data/models/User.ts
+++ data/models/User.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+import { User } from '../types';
 
 // scheme for user data (similar to ../types.ts)
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema<User>({
 	name: String,
 	password: String
 });
 
-// create model if doesnt already exist in cache; make it default export
-export const UserModel = mongoose.models.User || mongoose.model('users', UserSchema);
+// create model if doesnt already exist in cache
+export default (mongoose.models.user as mongoose.Model<User> || mongoose.model('user', UserSchema));
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { connect } from '../../data/connection';
-import { UserModel } from '../../data/models/User';
+import User from '../../data/models/User';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
@@ -12,13 +12,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const catcher = (error: Error) => res.status(400).json({ error });
 
   // using method to run database check/update
-  if(method=='GET') return res.json(await UserModel.find(req.body).catch(catcher)); // login
+  if(method=='GET') return res.json(await User.find(req.body).catch(catcher)); // login
   else if(method=='POST') {
     // checking if name already exists
-    if((await UserModel.find({name: req.body.name})).length > 0)
+    if((await User.find({name: req.body.name})).length > 0)
       return res.status(400).json({ error: 'user already exists' });
     
-    return res.json(await UserModel.create(req.body).catch(catcher)); // register
+    return res.json(await User.create(req.body).catch(catcher)); // register
   }
   else return res.status(400).json({ error: 'invalid request' }); // invalid req. error
 }
