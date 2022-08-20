9ab64a52c545ae2198610c1c4f417fd1a8fee3bf
voomp <bat7gamer7@gmail.com>
2022-08-20 19:38:53 -0400

    auth for deck/[id]

--- pages/deck/[id].tsx
+++ pages/deck/[id].tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react'
 import Navbar from '../../components/Navbar';
+import { isUserAuth } from '../../utils/auth';
 
 const Deck = () => {
+  
+  const router = useRouter();
+  useEffect(() => {
+    if(!isUserAuth(localStorage)) router.push('/register');
+  });
+  
   return (
     <div>
         <Navbar />
@@ -10,9 +18,9 @@ const Deck = () => {
   )
 }
 
-const getServerSideProps = (context) => {
-    console.log(context);
-    return { props: {} }
-}
+// const getServerSideProps = (context) => {
+//     console.log(context);
+//     return { props: {} }
+// }
 
 export default Deck
