260b1019aee6a11f28416afc60b10d9d248b7ad1
voomp <bat7gamer7@gmail.com>
2022-08-20 15:52:56 -0400

    api/register

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -11,8 +11,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // error handling function
   const catcher = (error: Error) => res.status(400).json({ error });
 
-  // using method to validate request
-  if(method=='POST')
-    return res.json(await User.find(req.body).catch(catcher)); // login
+  // using method to run database check/update
+  if(method=='POST') {
+    // checking if name already exists
+    if((await User.find({name: req.body.name})).length > 0)
+      return res.status(400).json({ error: 'user already exists' });
+    
+    return res.json(await User.create(req.body).catch(catcher)); // register
+  }
   else return res.status(400).json({ error: 'invalid request' }); // invalid req. error
 }
