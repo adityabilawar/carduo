0776ab1aa7c2dbe3b490833b8d5714e47827815a
aaronbiscotti <thesoupmanguy@gmail.com>
2022-08-21 01:26:09 -0400

    changes

--- components/Dashboard/Deck.tsx
+++ components/Dashboard/Deck.tsx
@@ -4,18 +4,20 @@ import { DeckData } from '../../data/Deck'
 
 const Deck = (props: { deck: DeckData, color: string }) => {
   return (
-    <Link href={`/deck/${props.deck.id}`}>
-      <div className="flex flex-col bg-[#DC3546] min-w-[330px] px-8 py-5 rounded m-2 cursor-pointer">
-          <div className="space-y-3 text-white mb-5">
-              <h1 className="text-2xl font-semibold">{props.deck.title}</h1>
-              <h1 className="text-sm">{props.deck.questions.length} cards in deck</h1>
-          </div>
-          <div className="border-t-2 border-[#AD0000] pt-5 text-white flex space-x-5 text-sm">
+    <div className="flex flex-col bg-[#DC3546] min-w-[330px] px-8 py-5 rounded m-2 cursor-pointer">
+        <div className="space-y-3 text-white mb-5">
+            <h1 className="text-2xl font-semibold">{props.deck.title}</h1>
+            <h1 className="text-sm">{props.deck.questions.length} cards in deck</h1>
+        </div>
+        <div className="border-t-2 border-[#AD0000] pt-5 text-white flex space-x-5 text-sm">
+          <Link href={`/deck/${props.deck.id}`}>
             <button>Study</button>
+          </Link>
+          <Link href="/room">
             <button>Teach It</button>
-          </div>
-      </div>
-    </Link>
+          </Link>
+        </div>
+    </div>
   )
 }
 
--- pages/room.tsx
+++ pages/room.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Room = () => {
   return (
-    <div>Room</div>
+    <div>
+        <Navbar />
+        <div className="p-20 text-2xl">
+            <h1>Teach It</h1>
+            <div className="text-lg mt-10 h-[600px] w-full bg-[#ECECEC] p-20 flex justify-around">
+                <div className="bg-[#D9D9D9] h-full mr-5 w-[40%]"></div>
+                <div className="bg-[#D9D9D9] h-full ml-5 w-[40%]"></div>
+            </div>
+            <button className="btn text-white mt-5">Next</button>
+        </div>
+    </div>
   )
 }
 
