69c1f342ad2d65b55db6463478344f7ed6a47fd8
voomp <bat7gamer7@gmail.com>
2022-08-21 07:56:05 -0400

    page header in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,19 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* App Header */}
+      <Head>
+        <title>Carduo</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
