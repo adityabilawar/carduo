da0583d80802bcac3844a7ffe306c86eaf5f718f
voomp <bat7gamer7@gmail.com>
2022-08-21 07:53:31 -0400

    cleaning auth util

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,3 +1,4 @@
+// checks if user is currently authorized
 export const isUserAuth = (store: Storage): boolean => {
 	const authData = store.getItem('auth');
 	if(!authData || authData==="") return false;
