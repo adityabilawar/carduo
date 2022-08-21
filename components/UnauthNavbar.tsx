d81440c699f7846ef03250b38f193c65296c3b07
voomp <bat7gamer7@gmail.com>
2022-08-21 07:40:37 -0400

    cleaning unauthnavbar comp

--- components/UnauthNavbar.tsx
+++ components/UnauthNavbar.tsx
@@ -1,12 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
 
+// navbar component (FOR UNAUTHORIZED USERS)
 const UnauthNavbar = () => {
   return (
     <div className="left-0 right-0 top-0 h-40 shadow flex px-10 fixed justify-between items-center bg-white">
+
+      {/* Navbar Logo */}
+      <Link href="/">
         <div className="flex justify-center items-center space-x-3">
-            <img src="/logo.svg" />
-            <h1 className="text-2xl font-bold">Carduo</h1>
+          <img src="/logo.svg" />
+          <h1 className="text-2xl font-bold">Carduo</h1>
         </div>
+      </Link>
+      
     </div>
   )
 }
